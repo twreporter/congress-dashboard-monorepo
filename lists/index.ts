9094eecf72a8558a12c256182dda82a90b335293
Lucien <liruchen0302@gmail.com>
2025-01-22 18:11:59 +0800

    feat: creat legislative meeting session list

--- lists/index.ts
+++ lists/index.ts
@@ -2,10 +2,12 @@ import SystemUser from './SystemUser'
 import Photo from './Photo'
 import Party from './Party'
 import LegislativeMeeting from './LegislativeMeeting'
+import LegislativeMeetingSession from './LegislativeMeetingSession'
 
 export const listDefinition = {
   SystemUser,
   Photo,
   LegislativeMeeting,
+  LegislativeMeetingSession,
   Party,
 }
