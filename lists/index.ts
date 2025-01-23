35aa65d927a7025d962f31c459173ea4e34e7eac
Lucien <liruchen0302@gmail.com>
2025-01-23 15:28:17 +0800

    feat: create legislative yuan member list

--- lists/index.ts
+++ lists/index.ts
@@ -4,12 +4,14 @@ import Party from './Party'
 import LegislativeMeeting from './LegislativeMeeting'
 import LegislativeMeetingSession from './LegislativeMeetingSession'
 import Legislator from './Legislator'
+import LegislativeYuanMember from './LegislativeYuanMember'
 
 export const listDefinition = {
   SystemUser,
   Photo,
   Legislator,
+  LegislativeYuanMember,
+  Party,
   LegislativeMeeting,
   LegislativeMeetingSession,
-  Party,
 }
