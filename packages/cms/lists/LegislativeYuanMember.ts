82defda30a05f724e678b534adf49e4274828519
Lucien <liruchen0302@gmail.com>
2025-02-07 17:22:59 +0800

    chore(shared): update for monorepo cross import

--- packages/cms/lists/LegislativeYuanMember.ts
+++ packages/cms/lists/LegislativeYuanMember.ts
@@ -6,7 +6,7 @@ import {
   MEMBER_TYPE_OPTIONS,
   CONSTITUENCY_OPTIONS,
   CITY_OPTIONS,
-} from '../../shared/constants/legislative-yuan-member'
+} from '@lucien-twreporter/congress-dashboard-shared/lib/constants/legislative-yuan-member'
 
 const listConfigurations = list({
   fields: {
---
+++ packages/shared/CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## 0.0.1, 2025-02-07 (Current)
+
+### Notable Changes
+
+- feat
+  - add constants for legislative-yuan-member
