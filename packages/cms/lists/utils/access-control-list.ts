651f5efb1ff95f41e5d3c519af3288673e090e33
Aylie Chou <aylie_chou@twreporter.org>
2025-03-28 15:36:22 +0800

    feat(cms): add `developer_headless_account` role
    - also add access control func for readonly role

--- packages/cms/lists/utils/access-control-list.ts
+++ packages/cms/lists/utils/access-control-list.ts
@@ -2,15 +2,24 @@ import envVars from '../../environment-variables'
 import {
   BaseListTypeInfo,
   ListOperationAccessControl,
+  MaybeItemFunction,
+  MaybeSessionFunction,
 } from '@keystone-6/core/types'
+import { get } from 'lodash'
+const _ = {
+  get,
+}
 
 export const RoleEnum = {
   Owner: 'owner',
   Admin: 'admin',
   Editor: 'editor',
+  Headless: 'developer_headless_account',
 } as const
 
 type AccessOperation = 'query' | 'create' | 'update' | 'delete'
+
+const readonlyRoles: string[] = [RoleEnum.Headless]
 
 export const allowRoles =
   <Operation extends AccessOperation>(
@@ -28,7 +37,12 @@ export const allowRoles =
   }
 
 export const allowAllRoles = () => {
-  const roles = [RoleEnum.Owner, RoleEnum.Admin, RoleEnum.Editor]
+  const roles = [
+    RoleEnum.Owner,
+    RoleEnum.Admin,
+    RoleEnum.Editor,
+    RoleEnum.Headless,
+  ]
   return allowRoles(roles)
 }
 
@@ -42,3 +56,31 @@ export const denyRoles =
     }
     return roles.indexOf(session?.data?.role) === -1
   }
+
+export const excludeReadOnlyRoles = () => {
+  return denyRoles(readonlyRoles)
+}
+
+type fieldMode = 'edit' | 'hidden' | 'read'
+export const withReadOnlyRoleFieldMode: MaybeItemFunction<
+  fieldMode,
+  BaseListTypeInfo
+> = ({ session }) => {
+  const role = _.get(session, ['data', 'role'], '')
+  if (readonlyRoles.indexOf(role) > -1) {
+    return 'read'
+  } else {
+    return 'edit'
+  }
+}
+
+export const hideReadOnlyRoles: MaybeSessionFunction<
+  boolean,
+  BaseListTypeInfo
+> = ({ session }) => {
+  const role = _.get(session, ['data', 'role'], '')
+  if (readonlyRoles.indexOf(role) > -1) {
+    return true
+  }
+  return false
+}
