cb060bb1a71b798e0c73eb2bffe9ae3eeb620023
Lucien <liruchen0302@gmail.com>
2025-04-10 12:00:36 +0800

    fix(frontend): use fetch for client SWR

--- packages/frontend/src/fetchers/legislative-meeting.ts
+++ packages/frontend/src/fetchers/legislative-meeting.ts
@@ -1,10 +1,71 @@
 'use client'
 import useSWR from 'swr'
 
-import {
-  fetchLegislativeMeeting,
-  fetchLegislativeMeetingSession,
+import type {
+  LegislativeMeeting,
+  LegislativeMeetingSession,
 } from '@/fetchers/server/legislative-meeting'
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL as string
+const fetchLegislativeMeeting = async (): Promise<LegislativeMeeting[]> => {
+  const res = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        query Query {
+          legislativeMeetings(orderBy: [{ term: desc }]) {
+            id
+            term
+          }
+        }
+      `,
+    }),
+  })
+  const data = await res.json()
+  return data?.data?.legislativeMeetings
+}
+
+const fetchLegislativeMeetingSession = async (
+  legislativeMeetingTerm: string
+): Promise<LegislativeMeetingSession[]> => {
+  const where = {
+    legislativeMeeting: {
+      term: {
+        equals: Number(legislativeMeetingTerm),
+      },
+    },
+  }
+
+  const orderBy = [{ term: 'asc' }]
+
+  const res = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        query LegislativeMeetingSessions($where: LegislativeMeetingSessionWhereInput!, $orderBy: [LegislativeMeetingSessionOrderByInput!]!) {
+          legislativeMeetingSessions(where: $where, orderBy: $orderBy) {
+            id
+            term
+            startTime
+            endTime
+          }
+        }
+      `,
+      variables: {
+        where,
+        orderBy,
+      },
+    }),
+  })
+  const data = await res.json()
+  return data?.data?.legislativeMeetingSessions
+}
 
 export const useLegislativeMeeting = () => {
   const { data, isLoading, error } = useSWR(
