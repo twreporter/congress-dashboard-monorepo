5779d322ba1dd43dbbbf98eeaf890cb055cf88be
nickhsine <nickhsine97753017@gmail.com>
2025-04-18 15:00:52 +0800

    feat(frontend): (desktop) instant search add compontents under src/components/search/ For better code review experience, I break the search feature into multiple commits.
    This commit only contains instant search of deskop version,
    not including search page.
    
    This feature provides:
    - customized search box
    - customized search hits
    - customized search logics

---
+++ packages/frontend/src/components/search/search-box.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useMemo } from 'react'
+import { useSearchBox } from 'react-instantsearch'
+import styled from 'styled-components'
+import { Search as IconSearch, X as IconX } from './icons'
+import debounce from 'lodash/debounce'
+
+const _ = {
+  debounce,
+}
+
+const Container = styled.div`
+  width: 100%;
+  background-color: #fff;
+
+  margin-bottom: 8px;
+  padding: 12px 20px;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 8px;
+
+  border: 1px solid #808080;
+  border-radius: 40px;
+`
+
+const Input = styled.input`
+  width: 100%;
+  font-size: 16px;
+  line-height: 150%;
+  border: 0;
+  outline: none;
+
+  color: #404040;
+`
+
+const ClearButton = styled.button`
+  background: none;
+  border: none;
+  padding: 0;
+  color: #888;
+  cursor: pointer;
+
+  line-height: 0;
+`
+
+export const SearchBox = () => {
+  const { query, refine: _refine } = useSearchBox()
+  const [inputValue, setInputValue] = useState(query)
+  const refine = useMemo(() => _.debounce(_refine, 500), [_refine])
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setInputValue(value)
+    refine(value)
+  }
+
+  const clearQuery = () => {
+    setInputValue('')
+    refine('')
+  }
+
+  return (
+    <Container>
+      <IconSearch />
+      <Input
+        type="text"
+        value={inputValue}
+        onChange={handleOnChange}
+        placeholder="搜尋立委、議題和逐字稿..."
+      />
+      {query && (
+        <ClearButton onClick={clearQuery}>
+          <IconX />
+        </ClearButton>
+      )}
+    </Container>
+  )
+}
