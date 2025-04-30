9816afffc96f0d164a4982827504313e04e21021
Lucien <liruchen0302@gmail.com>
2025-05-01 01:01:24 +0800

    chore(frontend): bump version to v1.0.0-beta.6

--- packages/frontend/src/app/api/graphql/keystone.ts
+++ packages/frontend/src/app/api/graphql/keystone.ts
@@ -8,25 +8,19 @@ export interface GraphQLResponse<T> {
 // 處理 graphql null -> undefined
 const transformNullToUndefined = <T>(obj: T): T => {
   if (Array.isArray(obj)) {
-    // 對於陣列，遞迴映射每個元素
     return obj.map(transformNullToUndefined) as T
   } else if (obj !== null && typeof obj === 'object') {
-    // 對於非 null 物件
     const result: Record<string, unknown> = {}
     for (const key in obj) {
-      // 只處理物件自身的屬性
       if (Object.prototype.hasOwnProperty.call(obj, key)) {
         const value = (obj as Record<string, unknown>)[key]
-        // 如果值不是 null，遞迴處理並加入結果
         if (value !== null) {
           result[key] = transformNullToUndefined(value)
         }
-        // 如果值是 null，則忽略，使其在結果中為 undefined
       }
     }
     return result as T
   }
-  // 對於非陣列、非物件或 null 的值，直接返回
   return obj
 }
 
