3928ca65537bb93db01f2ee79fe882ec8587ce84
Lucien <liruchen0302@gmail.com>
2025-05-07 15:46:44 +0800

    chore(frontend): change image & api host to new url

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
   },
   images: {
     domains: [
-      'dev-lawmaker-storage.twreporter.org',
-      'staging-lawmaker-storage.twreporter.org',
+      'dev-lawmaker.twreporter.org',
+      'staging-lawmaker.twreporter.org',
       'dev.twreporter.org',
       'staging.twreporter.org',
       'twreporter.org',
