1c687edb6004886210231267b4d63759ed248a74
Aylie Chou <aylie_chou@twreporter.org>
2025-05-09 02:10:09 +0800

    fix(frontend): resolve conflict

--- packages/frontend/src/components/sidebar/follow-more.tsx
+++ packages/frontend/src/components/sidebar/follow-more.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 // constant
-import { InternalRoutes } from '@/constants/navigation-link'
+import { InternalRoutes } from '@/constants/routes'
 // component
 import PartyTag from '@/components/dashboard/card/party-tag'
 import { CircleRaw } from '@/components/skeleton'
