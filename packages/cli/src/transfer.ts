75fcc34a2816f9c588943f8c7660f8e37fbbed73
nickhsine <nickhsine97753017@gmail.com>
2025-06-26 23:43:44 +0800

    feat: add packages/cli A command line tool to feed lawmaker data onto Algolia indices

---
+++ packages/cli/src/transfer.ts
@@ -0,0 +1,108 @@
+import type { LegislatorModel, TopicModel, SpeechModel } from './graphql'
+import type { LegislatorRecord, TopicRecord, SpeechRecord } from './algolia'
+import type { Constituency } from '@twreporter/congress-dashboard-shared/lib/constants/legislative-yuan-member'
+import { CONSTITUENCY_LABEL } from '@twreporter/congress-dashboard-shared/lib/constants/legislative-yuan-member'
+
+export function transferSpeechModelToRecord(
+  speechModels: SpeechModel[]
+): SpeechRecord[] {
+  return speechModels.map((s) => {
+    return {
+      slug: s.slug,
+      title: s.title,
+      date: s.date,
+      term: s.legislativeMeeting?.term,
+      session: s.legislativeMeetingSession?.term,
+      legislatorName: s.legislativeYuanMember?.legislator?.name,
+      summary: s.summary,
+    }
+  })
+}
+
+export function transferLegislatorModelToRecord(
+  legislatorModels: LegislatorModel[]
+): LegislatorRecord[] {
+  return legislatorModels.map((l) => {
+    const speechDate = l.speeches?.[0]?.date
+    let lastSpeechAt = ''
+    let desc = ''
+    if (l?.party?.name) {
+      desc = l.party.name.endsWith('籍')
+        ? `${l.party.name}。`
+        : `${l.party.name}籍。`
+    }
+    if (l?.legislativeMeeting?.term) {
+      desc = desc + `第${l.legislativeMeeting.term}屆立法委員`
+      // ts-ignore
+      const constituency = CONSTITUENCY_LABEL[l.constituency as Constituency]
+      if (constituency) {
+        desc = desc + `（${constituency}）。`
+      } else {
+        desc = desc + '。'
+      }
+    }
+
+    if (
+      Array.isArray(l.sessionAndCommittee) &&
+      l.sessionAndCommittee.length > 0
+    ) {
+      desc = desc + `加入：`
+      const committeeDesc: string[] = []
+      l.sessionAndCommittee.forEach((c) => {
+        const name = c.committee?.[0]?.name
+        const term = c.legislativeMeetingSession?.term
+        if (name && term) {
+          committeeDesc.push(`${name}（${term}會期）`)
+        } else if (name) {
+          committeeDesc.push(`${name}`)
+        }
+      })
+      desc = desc + committeeDesc.join('、') + '。'
+    }
+
+    if (l.note) {
+      desc = desc + l.note
+    }
+
+    if (speechDate) {
+      const date = new Date(speechDate)
+      lastSpeechAt = date.toISOString().split('T')[0].replace(/-/g, '/')
+    }
+
+    return {
+      name: l.legislator.name,
+      slug: l.legislator.slug,
+      term: l.legislativeMeeting.term,
+      lastSpeechAt,
+      desc,
+      imgSrc: l.legislator.imageLink || '',
+      partyImgSrc: l.party?.image?.imageFile?.url || '',
+      objectID: `${l.legislator.slug}_${l.legislativeMeeting.term}`,
+    }
+  })
+}
+
+export function transferTopicModelToRecord(
+  topicModels: TopicModel[]
+): TopicRecord[] {
+  return topicModels.map((t) => {
+    const countSum = t.members.reduce((sum: number, m: any) => sum + m.count, 0)
+    const desc = t.members.map((m: any) => `${m.name}(${m.count})`).join('、')
+    let lastSpeechAt = ''
+    if (t.topicInfo.lastSpeechAt) {
+      const date = new Date(t.topicInfo.lastSpeechAt)
+      lastSpeechAt = date.toISOString().split('T')[0].replace(/-/g, '/')
+    }
+
+    return {
+      name: t.topicInfo.title,
+      slug: t.topicInfo.slug,
+      term: t.topicInfo.meetingTerm,
+      session: t.topicInfo.sessionTerm,
+      desc,
+      lastSpeechAt,
+      relatedMessageCount: countSum,
+      objectID: `${t.topicInfo.slug}_${t.topicInfo.meetingTerm}_${t.topicInfo.sessionTerm}`,
+    }
+  })
+}
