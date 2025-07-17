6ae344337d31183735724d0ccc40f81177e7f077
nickhsine <nickhsine97753017@gmail.com>
2025-07-17 11:49:31 +0800

    refactor(frontend): (search) add src/components/search/index.tsx export `AlgoliaInstantSearch` and `SearchResults` components

---
+++ packages/frontend/src/components/search/index.tsx
@@ -0,0 +1,4 @@
+import { AlgoliaInstantSearch } from '@/components/search/instant-search'
+import { SearchResults } from '@/components/search/result-page/index'
+
+export { AlgoliaInstantSearch, SearchResults }
