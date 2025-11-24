2dda580deb40f4c834f055eb9d96c0033653bb47
Lucien <liruchen0302@gmail.com>
2025-11-24 20:13:08 +0800

    fix(frontend): test original logger

--- packages/frontend/src/utils/logger.ts
+++ packages/frontend/src/utils/logger.ts
@@ -1,19 +1,23 @@
-import pino from 'pino'
+import pino, { type Logger } from 'pino'
 import { createGcpLoggingPinoConfig } from '@google-cloud/pino-logging-gcp-config'
 
 const releaseBranch = process.env.NEXT_PUBLIC_RELEASE_BRANCH
 const isProduction = process.env.NODE_ENV === 'production'
 
-const logger = isProduction
+const logger: Logger = isProduction
   ? // Send logs to Google Cloud in production
-    pino({
-      ...createGcpLoggingPinoConfig({
-        serviceContext: {
-          service: `${releaseBranch}-congress-dashboard-frontend`,
+    pino(
+      createGcpLoggingPinoConfig(
+        {
+          serviceContext: {
+            service: `${releaseBranch}-congress-dashboard-frontend`,
+          },
         },
-      }),
-      level: 'debug',
-    })
+        {
+          level: 'debug',
+        }
+      )
+    )
   : pino({
       transport: {
         target: 'pino-pretty',
